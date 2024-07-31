6f7279030c2176a902f1a07b17591e1ad5bddf2d
Invitacion.Lolito <143892945+valentinasrgrsegrs@users.noreply.github.com>
2024-07-31 18:36:12 -0300

    initial commit

---
+++ prueba/script.js
@@ -0,0 +1,7 @@
+function cambiarColor() {
+    var colorTexto = "#ff0000"; // Color rojo
+    var colorFondo = "#ffff00"; // Color amarillo
+
+    document.getElementById("text").style.color = colorTexto;
+    document.body.style.backgroundColor = colorFondo;
+}
