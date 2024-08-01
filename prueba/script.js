adf5a11131b6a549d8071912c00eac833085d8e1
Invitacion.Lolito <143892945+valentinasrgrsegrs@users.noreply.github.com>
2024-07-31 21:16:04 -0300

    Delete prueba directory

--- prueba/script.js
+++
@@ -1,7 +0,0 @@
-function cambiarColor() {
-    var colorTexto = "#ff0000"; // Color rojo
-    var colorFondo = "#ffff00"; // Color amarillo
-
-    document.getElementById("text").style.color = colorTexto;
-    document.body.style.backgroundColor = colorFondo;
-}
